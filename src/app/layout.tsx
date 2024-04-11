bf5e4c0c5e94fc80235f2c064ea7c268c84beab8
Ugo Romi <ugo.romi@gmail.com>
2024-04-11 20:54:18 +0200

    added Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -19,6 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={cn(
@@ -27,5 +29,6 @@ export default function RootLayout({
         )}
       >{children}</body>
     </html>
+    </ClerkProvider>
   );
 }
