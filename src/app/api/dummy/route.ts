d1eba82f83958b3473d33ff611584ca0f1f5ca5d
BennyTaccardi <benny.taccardi@gmail.com>
2024-04-15 00:30:35 +0200

    Testing and validating Turso

--- src/app/api/dummy/route.ts
+++ src/app/api/dummy/route.ts
@@ -1,18 +1,15 @@
 import { client } from "@/db/client";
 import { NextRequest } from "next/server";
 
-export async function POST(request: NextRequest): Promise<boolean> {
+export async function POST(request: NextRequest): Promise<Response> {
   const { action, payload } = await request.json();
-  console.log(JSON.stringify(payload));
   if (action === "add") {
-    try {
-      const result = await client.execute("SELECT * from users");
-      console.log(result);
-      return true;
-    } catch (e) {
-      console.error(e);
-      return false;
-    }
+    const result = await client.execute("SELECT * from gifter");
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+    });
   }
-  return true;
+  return new Response("Action not supported", {
+    status: 500,
+  });
 }
