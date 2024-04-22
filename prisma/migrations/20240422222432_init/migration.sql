4b0c5cec9b578984d8e678bd68cc3d6dbd2f9bff
BennyTaccardi <benny.taccardi@gmail.com>
2024-04-23 00:29:34 +0200

    prisma + migrations

---
+++ prisma/migrations/20240422222432_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "storeDescription" TEXT
+);
---
+++ prisma/migrations/20240422222913_test/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Merchant" ADD COLUMN "url" TEXT;
