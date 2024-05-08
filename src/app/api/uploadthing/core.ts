bd18756bb92b0134cb284a46d0ea4af2bceee272
BennyTaccardi <benny.taccardi@gmail.com>
2024-05-09 00:22:35 +0200

    Playing with uploadthing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,9 +1,13 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const auth = async (req: Request) => {
+  const loggedUser = await currentUser();
+  return loggedUser;
+}; // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
